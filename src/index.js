4f208158010f077ecfb4ec857454160eaeb3d7e1
anurag2541997 <a.dixit@iiitmanipur.ac.in>
2020-09-11 17:55:02 +0530

    Second Commit

--- src/index.js
+++ src/index.js
@@ -16,6 +16,28 @@ app.route('/').get((req,res)=>{
     return res.json("Hello World!")
 })
 
+//Addition
+
+app.route('/add').post((req,res)=>{
+    const {num1,num2}=req.body;
+    if(typeof(num1)=="string" || typeof (num2)=="string")
+    {
+        return res.json({
+            message : "invalid data types"
+        })
+    }
+    if(num1>1000000 || num2> 1000000)
+    {
+        return res.json({
+            message :"Overflow"
+        })
+    }
+    return res.json({
+        status :"success/failure/error",
+        message : "the sum of given two number",
+        sum : num1+num2
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
